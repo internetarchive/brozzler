8eb92b28e60656673401501ff19579b2f0c82acf
Noah Levitt <nlevitt@archive.org>
2014-01-27 19:34:54 -0800

    make load_url handle arguments similarly to umbra

--- bin/load_url.py
+++ bin/load_url.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# vim: set sw=4 et:
 from json import dumps, loads
 import os,sys,argparse, urllib.request, urllib.error, urllib.parse
 import websocket
@@ -7,10 +8,21 @@ import uuid
 import logging
 import threading
 from kombu import Connection, Exchange, Queue
-logging.basicConfig(level=logging.INFO)
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+        format='%(asctime)s %(process)d %(levelname)s %(threadName)s %(name)s.%(funcName)s(%(filename)s:%(lineno)d) %(message)s')
+
+arg_parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]),
+        description='load_url.py - send url to umbra via amqp',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+arg_parser.add_argument('-u', '--url', dest='amqp_url', default='amqp://guest:guest@localhost:5672//',
+        help='URL identifying the amqp server to talk to')
+arg_parser.add_argument('urls', metavar='URL', nargs='+', help='URLs to send to umbra')
+args = arg_parser.parse_args(args=sys.argv[1:])
+
 umbra_exchange = Exchange('umbra', 'direct', durable=True)
-with Connection(sys.argv[1]) as conn:
+with Connection(args.amqp_url) as conn:
     producer = conn.Producer(serializer='json')
-    for url in sys.argv[2:]:
+    for url in args.urls:
         producer.publish({'url': url}, 'url', exchange=umbra_exchange)
         
