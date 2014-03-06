f2f78d2ced740a5f63cd8083bba7958610142ef6
Eldon <eldon@archive.org>
2014-03-05 23:19:09 -0500

    Convert from one big json file, to js files with a regex as a comment at the top.

---
+++ umbra/behaviors.d/facebook.js
@@ -0,0 +1,52 @@
+//.*facebook.com.* 
+var isOnScreen = function(e) {
+        var eTop = e.getBoundingClientRect().top;
+        return eTop > window.scrollY && eTop < window.scrollY + window.innerHeight;
+};
+ 
+var THINGS_TO_CLICK_SELECTOR = 'a.UFIPagerLink > span, a.UFIPagerLink, a[href^="/browse/likes"], span.UFIReplySocialSentenceLinkText';
+var alreadyClicked = {};
+var intervalId;
+ 
+var intervalFunc = function() {
+        var closeButton = document.querySelector('a[title="Close"]');
+        if (closeButton) { 
+                console.log("clicking close button " + closeButton);
+                closeButton.click();
+                return;
+        }
+        
+        var thingsToClick = document.querySelectorAll(THINGS_TO_CLICK_SELECTOR);
+        var clickedSomething = false;
+        var somethingLeftToClick = false;
+ 
+        for (var i = 0; i < thingsToClick.length; i++) {
+                var target = thingsToClick[i]; 
+                if (!(target in alreadyClicked)) {
+                        if (isOnScreen(target)) {
+                                // var pos = target.getBoundingClientRect().top;
+                                // window.scrollTo(0, target.getBoundingClientRect().top - 100);
+                                console.log("clicking at " + target.getBoundingClientRect().top + " on " + target);
+                                target.click();
+                                target.style.border = '1px solid #0a0';
+                                alreadyClicked[target] = true;
+                                clickedSomething = true;
+                                break;
+                        } else {
+                                somethingLeftToClick = true;
+                        }
+                }
+        }
+ 
+        if (!clickedSomething) {
+                if (somethingLeftToClick) {
+                        console.log("scrolling because everything on this screen has been clicked but there's more below document.body.clientHeight=" + document.body.clientHeight);
+                        window.scrollBy(0, 100);
+                } else if (window.scrollY + window.innerHeight + 10 < document.body.clientHeight) {
+                        console.log("scrolling because we're not to the bottom yet document.body.clientHeight=" + document.body.clientHeight);
+                        window.scrollBy(0, 100);
+                } 
+        }
+}
+ 
+var intervalId = setInterval(intervalFunc, 200);
--- umbra/behaviors.py
+++ umbra/behaviors.py
@@ -2,17 +2,25 @@
 
 from json import dumps, load
 from time import sleep
+from itertools import chain
 import os, re
 import logging
 
-behaviors_file = os.path.sep.join(__file__.split(os.path.sep)[:-1] + ['behaviors.json'])
+behaviors_directory = os.path.sep.join(__file__.split(os.path.sep)[:-1] + ['behaviors.d'])
+behavior_files = chain(*[[dir + os.path.sep + file for file in files] for dir, dirs, files  in os.walk(behaviors_directory)])
+behaviors = []
+for file_name in behavior_files:
+    lines = open(file_name).readlines()
+    pattern, script = lines[0][2:].strip(), ''.join(lines[1:])
+    behaviors.append({'site' : pattern, 'script': script})
+
+print(behaviors)
 def execute(url, websock, command_id):
     logger = logging.getLogger('behaviors')
-    with open(behaviors_file) as js:
-        behaviors = load(js)
-        for behavior in behaviors:
-            if re.match(behavior['site'], url):
-                for script in behavior['scripts']:
-                    msg = dumps(dict(method="Runtime.evaluate", params={"expression": script}, id=next(command_id)))
-                    logger.debug('sending message to {}: {}'.format(websock, msg))
-                    websock.send(msg)
+    print(behaviors)
+    for behavior in behaviors:
+        print("Comparing %s and %s" %(behavior['site'], url))
+        if re.match(behavior['site'], url):
+            msg = dumps(dict(method="Runtime.evaluate", params={"expression": behavior['script']}, id=next(command_id)))
+            logger.debug('sending message to {}: {}'.format(websock, msg))
+            websock.send(msg)
