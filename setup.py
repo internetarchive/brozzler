bef57e2819748f301ed285e9d6f6e3b9a5e2c9ee
Noah Levitt <nlevitt@archive.org>
2014-05-29 20:57:33 -0700

    for version string, try to handle case where head is detached

--- setup.py
+++ setup.py
@@ -8,12 +8,15 @@ VERSION_BYTES = b'0.2'
 def full_version_bytes():
     import subprocess, time
     try:
-        git_branch = subprocess.check_output(['git', 'symbolic-ref', '--short', 'HEAD'])
+        git_status = subprocess.check_output(['git', 'status'])
+        line1 = git_status[:git_status.find(b'\n')]
+        git_head = line1.split()[-1]
+
         t_bytes = subprocess.check_output(['git', 'log', '-1', '--pretty=format:%ct'])
         t = int(t_bytes.strip().decode('utf-8'))
         tm = time.gmtime(t)
         timestamp_utc = time.strftime("%Y%m%d%H%M%S", time.gmtime(t))
-        return VERSION_BYTES + b'-' + git_branch.strip() + b'-' + timestamp_utc.encode('utf-8')
+        return VERSION_BYTES + b'-' + git_head.strip() + b'-' + timestamp_utc.encode('utf-8')
     except subprocess.CalledProcessError:
         return VERSION_BYTES
 
