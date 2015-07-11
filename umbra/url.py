1fb336cb2ee375155af270bbb43c19a92930203c
Noah Levitt <nlevitt@archive.org>
2015-07-11 02:29:19 -0700

    crawling outlinks not totally working

--- umbra/url.py
+++ umbra/url.py
@@ -10,11 +10,17 @@ class CrawlUrl:
         self.url = url
         self.hops_from_seed = hops_from_seed
         self._canon_hurl = None
-        self.outlinks = None
+        self.outlinks = outlinks
 
     def __repr__(self):
         return """CrawlUrl(url="{}",site_id={},hops_from_seed={})""".format(
                 self.url, self.site_id, self.hops_from_seed)
+
+    def calc_priority(self):
+        priority = 0
+        priority += max(0, 10 - self.hops_from_seed)
+        priority += max(0, 6 - self.canonical().count("/"))
+        return priority
 
     def canonical(self):
         if self._canon_hurl is None:
@@ -22,14 +28,16 @@ class CrawlUrl:
             surt.GoogleURLCanonicalizer.canonicalize(self._canon_hurl)
         return self._canon_hurl.geturl()
 
-    def to_json(self):
+    def to_dict(self):
         if self.outlinks is not None and not isinstance(self.outlinks, list):
             outlinks = []
             outlinks.extend(self.outlinks)
         else:
             outlinks = self.outlinks
 
-        d = dict(id=self.id, site_id=self.site_id, url=self.url,
+        return dict(id=self.id, site_id=self.site_id, url=self.url,
                 hops_from_seed=self.hops_from_seed, outlinks=outlinks)
-        return json.dumps(d, separators=(',', ':'))
+
+    def to_json(self):
+        return json.dumps(self.to_dict(), separators=(',', ':'))
 
