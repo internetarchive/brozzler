18ca996216135a4c6e9ef38321285fca1397a05e
Noah Levitt <nlevitt@archive.org>
2015-07-13 15:56:54 -0700

    rudimentary robots.txt support

--- umbra/hq.py
+++ umbra/hq.py
@@ -4,6 +4,28 @@ import surt
 import kombu
 import json
 import logging
+import urllib.robotparser
+
+# robots_url : RobotsFileParser
+_robots_cache = {}
+def robots(robots_url):
+    if not robots_url in _robots_cache:
+        robots_txt = urllib.robotparser.RobotFileParser(robots_url)
+        logging.info("fetching {}".format(robots_url))
+        try:
+            robots_txt.read() # XXX should fetch through proxy
+            _robots_cache[robots_url] = robots_txt
+        except BaseException as e:
+            logger.error("problem fetching {}".format(robots_url))
+
+    return _robots_cache[robots_url]
+
+def robots_url(url):
+    hurl = surt.handyurl.parse(url)
+    hurl.path = "/robots.txt"
+    hurl.query = None
+    hurl.hash = None
+    return hurl.geturl()
 
 class Site:
     logger = logging.getLogger(__module__ + "." + __qualname__)
@@ -13,12 +35,15 @@ class Site:
         self.id = id
         self.scope_surt = surt.surt(seed, canonicalizer=surt.GoogleURLCanonicalizer, trailing_comma=True)
 
+    def is_permitted_by_robots(self, url):
+        return robots(robots_url(url)).can_fetch("*", url)
+
     def is_in_scope(self, url):
         try:
             surtt = surt.surt(url, canonicalizer=surt.GoogleURLCanonicalizer, trailing_comma=True)
             return surtt.startswith(self.scope_surt)
         except:
-            self.logger.warn("""problem parsing url "{}" """.format(url), exc_info=True)
+            self.logger.warn("""problem parsing url "{}" """.format(url))
             return False
 
     def to_dict(self):
@@ -26,4 +51,4 @@ class Site:
 
     def to_json(self):
         return json.dumps(self.to_dict(), separators=(',', ':'))
-        
+
