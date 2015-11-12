21906f8cad26e733cc2afa2fa351985da61795d6
Noah Levitt <nlevitt@archive.org>
2015-11-12 02:59:45 +0000

    vnc-websock.sh uses bashisms

--- no-docker/vnc-websock.sh
+++ no-docker/vnc-websock.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
+#!/bin/bash
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PYTHONPATH=/home/nlevitt/workspace/websockify/websockify-ve34/lib/python3.4/site-packages:/home/nlevitt/workspace/websockify exec /home/nlevitt/workspace/websockify/websockify-ve34/bin/websockify 0.0.0.0:8901 localhost:5901 >> /home/nlevitt/workspace/brozzler/no-docker/websockify-`hostname -s`.out 2>&1
