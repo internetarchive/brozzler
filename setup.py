b2d4cc5ff0eca34101ec970bc8f7160c82d9a56e
Noah Levitt <nlevitt@archive.org>
2016-06-16 14:11:45 -0500

    beta version number for pypi upload

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import glob
 
 setuptools.setup(
         name='brozzler',
-        version='1.1.dev13',
+        version='1.1b1',
         description='Distributed web crawling with browsers',
         url='https://github.com/nlevitt/brozzler',
         author='Noah Levitt',
