81d709eed06526cbbafb89b6ed205fcec487d860
Noah Levitt <nlevitt@archive.org>
2016-06-16 13:56:08 -0500

    bump version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import glob
 
 setuptools.setup(
         name='brozzler',
-        version='1.1.dev12',
+        version='1.1.dev13',
         description='Distributed web crawling with browsers',
         url='https://github.com/nlevitt/brozzler',
         author='Noah Levitt',
