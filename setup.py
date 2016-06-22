366e46750156948ece4b1874bc7ce306406f142a
Noah Levitt <nlevitt@archive.org>
2016-06-22 23:09:27 +0000

    enhancements to the page thumbnail on the site page

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import glob
 
 setuptools.setup(
         name='brozzler',
-        version='1.1.dev19',
+        version='1.1.dev20',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
