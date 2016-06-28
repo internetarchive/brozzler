0bd687abde1c1bd7c9e0dd74daa22916efda1469
Noah Levitt <nlevitt@archive.org>
2016-06-28 11:25:04 -0500

    avoid hanging in case a page has no outlinks

--- brozzler/browser.py
+++ brozzler/browser.py
@@ -273,7 +273,7 @@ class Browser:
                 or self._waiting_on_screenshot_msg_id):
             return False
 
-        if self._outlinks:
+        if self._outlinks is not None:
             self.logger.info("got outlinks, finished browsing %s", self.url)
             return True
         elif not self._waiting_on_outlinks_msg_id:
