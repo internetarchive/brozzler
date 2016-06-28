6a11e1da2af94f1b17dc70ade7dc88a9ff6ecdcc
Noah Levitt <nlevitt@archive.org>
2016-06-28 16:15:16 +0000

    fix noVNC submodule path since brozzler webconsole has moved

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import setuptools
 
 setuptools.setup(
         name='brozzler',
-        version='1.1.dev23',
+        version='1.1.dev24',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
