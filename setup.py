041a4970ceb8d3dd929ad7c628ddbf2190f4b81c
Noah Levitt <nlevitt@archive.org>
2016-07-27 16:57:42 -0500

    back to a dev version number

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ import setuptools
 
 setuptools.setup(
         name='brozzler',
-        version='1.1b3',
+        version='1.1b4.dev59',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
