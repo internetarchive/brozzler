57c0d84fbd7e36b63ca78c1da4465523b18bff33
Noah Levitt <nlevitt@archive.org>
2016-08-04 15:55:56 -0700

    bump version to 1.1b4 for pypi upload

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def find_package_data(package):
 
 setuptools.setup(
         name='brozzler',
-        version='1.1b4.dev65',
+        version='1.1b4',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
