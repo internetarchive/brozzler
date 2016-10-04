15633be612fa83d406fa300d323987805dd504a0
Noah Levitt <nlevitt@archive.org>
2016-10-03 18:17:35 -0700

    finish vagrant-brozzler-new-job.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def find_package_data(package):
 
 setuptools.setup(
         name='brozzler',
-        version='1.1b6.dev90',
+        version='1.1b6.dev91',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
