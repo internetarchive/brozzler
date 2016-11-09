02bf23059e090bfbfb88deecd70cef7d7b752552
Noah Levitt <nlevitt@archive.org>
2016-11-09 13:43:10 -0800

    pass behavior_parameters from job configuration into Site objects

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def find_package_data(package):
 
 setuptools.setup(
         name='brozzler',
-        version='1.1b7.dev120',
+        version='1.1b7.dev121',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
