a74247412c4f9828c42a5a5b49846126fee88593
Noah Levitt <nlevitt@archive.org>
2016-11-14 10:03:40 -0800

    need warcprox to listen on public address because that's what it puts in the service registry

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def find_package_data(package):
 
 setuptools.setup(
         name='brozzler',
-        version='1.1b8.dev123',
+        version='1.1b8.dev124',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
