65f818e9015c4b95fbe3de1f2e56460fb6008886
Noah Levitt <nlevitt@archive.org>
2017-01-16 12:44:12 -0800

    add travis-ci slack notification to internetarchive/brozzler channel

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def find_package_data(package):
 
 setuptools.setup(
         name='brozzler',
-        version='1.1b9.dev169',
+        version='1.1b9.dev170',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
