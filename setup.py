613dca29dcba464600183fa68ab68bc64bf8b100
Noah Levitt <nlevitt@archive.org>
2017-03-22 16:11:26 -0700

    1.1b10 since 1.1b9 has bugs :(

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def find_package_data(package):
 
 setuptools.setup(
         name='brozzler',
-        version='1.1b10.dev217',
+        version='1.1b10',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
