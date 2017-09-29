e6bb6791af9c28d840912d559bcef70ba2e707cb
Barbara Miller <galgeek@me.com>
2017-09-29 14:53:24 -0700

    skip unnecessary assignment

--- brozzler/cli.py
+++ brozzler/cli.py
@@ -193,10 +193,9 @@ def brozzle_page(argv=None):
     browser = brozzler.Browser(chrome_exe=args.chrome_exe)
     try:
         browser.start(proxy=args.proxy)
-        enable_youtube_dl = False if args.skip_youtube_dl else True
         outlinks = worker.brozzle_page(
                 browser, site, page, on_screenshot=on_screenshot,
-                enable_youtube_dl=enable_youtube_dl)
+                enable_youtube_dl=not args.skip_youtube_dl)
         logging.info('outlinks: \n\t%s', '\n\t'.join(sorted(outlinks)))
     except brozzler.ReachedLimit as e:
         logging.error('reached limit %s', e)
