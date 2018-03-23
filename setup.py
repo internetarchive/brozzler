60f2b99cc0c7eb0d6593d59e1d85487e8b030c73
Noah Levitt <nlevitt@archive.org>
2018-03-22 17:06:46 -0700

    doublethink had a bug fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 '''
 setup.py - brozzler setup script
 
-Copyright (C) 2014-2017 Internet Archive
+Copyright (C) 2014-2018 Internet Archive
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -70,7 +70,7 @@ setuptools.setup(
             'websocket-client!=0.39.0',
             'pillow==3.3.0',
             'urlcanon>=0.1.dev16',
-            'doublethink>=0.2.0.dev81',
+            'doublethink>=0.2.0.dev88',
             'rethinkdb>=2.3,<2.4',
             'cerberus==1.0.1',
             'jinja2',
