1073431f76f7d427c1af7b4acaa3b214d28e7f82
Noah Levitt <nlevitt@archive.org>
2018-10-29 17:42:25 -0700

    handle exceptions extracting links
    like this one:
    Uncaught DOMException: Blocked a frame with origin "https://www.youtube.com" from accessing a cross-origin frame.
        at __brzl_compileOutlinks (<anonymous>:4:24)
        at __brzl_compileOutlinks (<anonymous>:10:29)
        at <anonymous>:16:1
    __brzl_compileOutlinks @ VM194:4
    __brzl_compileOutlinks @ VM194:10
    
    not sure exactly why this happens but we just have to handle it

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def find_package_data(package):
 
 setuptools.setup(
         name='brozzler',
-        version='1.5.dev311',
+        version='1.5.dev312',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
