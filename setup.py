ebcc063fe28e4e6d1e590787be3676a358832d0e
Noah Levitt <nlevitt@archive.org>
2018-11-29 14:52:11 -0800

    bump version after merge

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def find_package_data(package):
 
 setuptools.setup(
         name='brozzler',
-        version='1.5.dev314',
+        version='1.5.dev315',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
