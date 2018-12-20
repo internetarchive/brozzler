034f7938c4bf608a0e56283f741da9a6451777b8
Noah Levitt <nlevitt@archive.org>
2018-12-20 10:46:05 -0800

    catch common exception in default behavior

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def find_package_data(package):
 
 setuptools.setup(
         name='brozzler',
-        version='1.5.dev318',
+        version='1.5.dev319',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
