4d508cc55cb7299e4103801fb9e6917b735c43cd
Barbara Miller <galgeek@me.com>
2019-05-14 11:59:59 -0700

    log Network.requestIntercepted

--- brozzler/browser.py
+++ brozzler/browser.py
@@ -241,6 +241,12 @@ class WebsockReceiverThread(threading.Thread):
             elif message['method'] == 'Network.requestWillBeSent':
                 if self.on_request:
                     self.on_request(message)
+            elif message['method'] == 'Network.requestIntercepted':
+                if 'params' in message:
+                    for p in message['params']:
+                        self.logger.info(
+                            'Network.requestIntercepted param  %s: %s',
+                            p, message['params'][p])
             elif message['method'] == 'Page.interstitialShown':
                 # AITFIVE-1529: handle http auth
                 # we should kill the browser when we receive Page.interstitialShown and
@@ -351,6 +357,10 @@ class Browser:
             self.send_to_chrome(method='Page.enable')
             self.send_to_chrome(method='Console.enable')
             self.send_to_chrome(method='Runtime.enable')
+            self.send_to_chrome(
+                method='Network.setRequestInterception',
+                params={'patterns': [{'urlPattern': '*'}]})
+
             self.send_to_chrome(method='ServiceWorker.enable')
             self.send_to_chrome(method='ServiceWorker.setForceUpdateOnPageLoad')
 
