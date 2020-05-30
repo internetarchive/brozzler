93b3fb7855471da6cbbfcb671d88cbc387b6837f
Barbara Miller <galgeek@me.com>
2020-05-29 17:20:11 -0700

    update copyright

--- brozzler/ydl.py
+++ brozzler/ydl.py
@@ -1,7 +1,7 @@
 '''
 brozzler/ydl.py - youtube-dl support for brozzler
 
-Copyright (C) 2018 Internet Archive
+Copyright (C) 2020 Internet Archive
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
