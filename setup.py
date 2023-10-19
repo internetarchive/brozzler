6c4a9c0f897cd34a8fef0149beb7b7a54bb0c6c2
Barbara Miller <3253863+galgeek@users.noreply.github.com>
2023-10-19 10:24:20 -0700

    bump version

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ def find_package_data(package):
 
 setuptools.setup(
         name='brozzler',
-        version='1.5.38',
+        version='1.5.39',
         description='Distributed web crawling with browsers',
         url='https://github.com/internetarchive/brozzler',
         author='Noah Levitt',
