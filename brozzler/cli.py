5138fe95846b0982c8d100a8afbd0a0a18916b1f
Adam Miller <adam@archive.org>
2024-09-24 16:25:46 -0700

    chore: formatting

--- brozzler/cli.py
+++ brozzler/cli.py
@@ -450,10 +450,14 @@ def brozzler_new_page(argv=None):
         formatter_class=BetterArgumentDefaultsHelpFormatter,
     )
     arg_parser.add_argument("url", metavar="URL", help="URL to add to site")
-    arg_parser.add_argument("site_id", metavar="SITE_ID",
-                            help="UUID of site object to add the page to")
-    arg_parser.add_argument("parent_page_id", metavar="PARENT_PAGE_ID",
-                            help="ID of Page object to add the page as an outlink to")
+    arg_parser.add_argument(
+        "site_id", metavar="SITE_ID", help="UUID of site object to add the page to"
+    )
+    arg_parser.add_argument(
+        "parent_page_id",
+        metavar="PARENT_PAGE_ID",
+        help="ID of Page object to add the page as an outlink to",
+    )
     add_rethinkdb_options(arg_parser)
     add_common_options(arg_parser, argv)
     args = arg_parser.parse_args(args=argv[1:])
@@ -472,7 +476,9 @@ def brozzler_new_page(argv=None):
     parent_page = brozzler.Page(rr, parent_page_result)
 
     frontier = brozzler.RethinkDbFrontier(rr)
-    frontier.scope_and_schedule_outlinks(site=site, parent_page=parent_page, outlinks=[args.url])
+    frontier.scope_and_schedule_outlinks(
+        site=site, parent_page=parent_page, outlinks=[args.url]
+    )
 
 
 def brozzler_worker(argv=None):
@@ -1103,7 +1109,7 @@ def brozzler_list_captures(argv=None):
         reql = reql.order_by(index="abbr_canon_surt_timestamp")
         reql = reql.filter(
             lambda capture: (capture["canon_surt"] >= key)
-                            & (capture["canon_surt"] <= end_key)
+            & (capture["canon_surt"] <= end_key)
         )
         logging.debug("querying rethinkdb: %s", reql)
         results = reql.run()
