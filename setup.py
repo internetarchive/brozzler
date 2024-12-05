42e2452363f298a21beb885f04074e8efa97ad52
Adam Miller <adam@archive.org>
2024-12-05 09:14:11 -0800

    chore: Bump version to 1.6.4

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def find_package_data(package):
 
 setuptools.setup(
     name="brozzler",
-    version="1.6.3",
+    version="1.6.4",
     description="Distributed web crawling with browsers",
     url="https://github.com/internetarchive/brozzler",
     author="Noah Levitt",
