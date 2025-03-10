d1b2fad9ce1b8d1cc72cd628469abddff7de6ea8
Gretchen Miller <gretchen@gretchenleigh.zone>
2025-03-10 09:12:23 -0700

    fix logging calls to use structlog

--- brozzler/worker.py
+++ brozzler/worker.py
@@ -312,12 +312,12 @@ class BrozzlerWorker:
         if not self._needs_browsing(page_headers):
             page_logger.info("needs fetch")
             if site.pdfs_only and not self._is_pdf(page_headers):
-                self.logger.info("skipping non-PDF content: PDFs only option enabled")
+                page_logger.info("skipping non-PDF content: PDFs only option enabled")
             elif site.video_capture in [
                 VideoCaptureOptions.DISABLE_VIDEO_CAPTURE.value,
                 VideoCaptureOptions.BLOCK_VIDEO_MIME_TYPES.value,
             ] and self._is_video_type(page_headers):
-                self.logger.info(
+                page_logger.info(
                     "skipping video content: video MIME type capture disabled for site"
                 )
             else:
